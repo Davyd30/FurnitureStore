b11c8c1a5246f7cf1256b47ec176f3dfa4864c72
DavydSafonov <safonovdavid30@gmail.com>
2025-11-13 12:13:22 +0000

    Initial Angular project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/pages/home/home.component').then(m => m.HomeComponent),
+  },
+  {
+    path: 'catalog',
+    loadComponent: () =>
+      import('./features/catalog/pages/catalog/catalog.component').then(m => m.CatalogComponent),
+  },
+  { path: '**', redirectTo: '' }
+];
